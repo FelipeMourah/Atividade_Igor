093ef5e325d550332f5406fd1f5274b468c4fea9
Felipe Moura <110267725+FelipeMourah@users.noreply.github.com>
2024-05-30 19:53:58 -0300

    Delete atividade_igor directory

--- atividade_igor/android/app/src/main/kotlin/com/example/atividade_igor/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.atividade_igor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- atividade_igor/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:atividade_igor/pages/home.dart';
-import 'package:flutter/material.dart';
-void main() {
-  runApp(const MaterialApp(home:homePage()));
-}
-
-
